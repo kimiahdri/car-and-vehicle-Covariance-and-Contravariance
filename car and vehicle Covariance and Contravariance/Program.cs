7392757a252d1c408721bdc5320c28639dea18db
kimi <kimi@DESKTOP-HLSO57E>
2022-12-18 13:28:23 +0330

    Add project files.

---
+++ car and vehicle Covariance and Contravariance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace car_and_vehicle_Covariance_and_Contravariance
+{
+    class Vehicle
+    { 
+    
+    }
+
+    class Car : Vehicle
+    { 
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Car();
+            Vehicle v2 = new Car();
+
+            Func<Vehicle> func = GetVehicle;
+            Vehicle v3 = func();
+
+            Action<Car> action = parkvehicle;
+            action(new Car());
+        }
+
+        static Car GetVehicle()
+        {
+            return new Car();
+        }
+
+        static void parkvehicle(Vehicle vehicle)
+        { 
+        
+        }
+    }
+}
